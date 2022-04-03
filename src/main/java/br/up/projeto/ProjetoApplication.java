2f4690f513a817699a484ba4a3207268b659f195
FelipeMacchiz <felipe.macchiz@gmail.com>
2022-04-03 19:27:46 -0300

    Update ProjetoApplication.java

--- src/main/java/br/up/projeto/ProjetoApplication.java
+++ src/main/java/br/up/projeto/ProjetoApplication.java
@@ -15,13 +15,7 @@ public class ProjetoApplication {
 
     @RequestMapping("/")
     public String home() {
-        return  "<a href='localhost:8080/'>Home</a><br/>" +
-                "<a href='localhost:8080/contas'>Contas</a><br/>" +
-                "<a href='localhost:8080/lancamento-financeiro'>Lançamento financeiro</a><br/>" +
-                "<a href='localhost:8080/meio-de-pagamento'>Meio de pagamento</a><br/>" +
-                "<a href='localhost:8080/operadora'>Operadora (bandeira)</a><br/>" +
-                "<a href='localhost:8080/moedas'>Índices e moedas</a><br/>" +
-                "<a href='localhost:8080/cliente'>Cliente</a><br/>";
+        return  "<a href='localhost:8080/'>Home</a><br/>";
     }
 
 }
